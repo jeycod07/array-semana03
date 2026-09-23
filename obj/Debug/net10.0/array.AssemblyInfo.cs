84dd7e382ac0d391e4f4f974e274a266f0a9cd0c
Joaquin sanchez <joaquinsanchezb2007@gmail.com>
2026-09-23 16:43:34 -0500

    Funcion de nota máxima

--- obj/Debug/net10.0/array.AssemblyInfo.cs
+++ obj/Debug/net10.0/array.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("array")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a8fcc5b2a8121738512649bed68e2aed91c1b19")]
 [assembly: System.Reflection.AssemblyProductAttribute("array")]
 [assembly: System.Reflection.AssemblyTitleAttribute("array")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
